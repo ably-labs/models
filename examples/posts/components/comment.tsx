1fe208a34f7ceb20dff057dbbb3cc1a33f4d8fd1
Mike Christensen <mike.christensen@ably.com>
2023-07-11 16:32:29 +0100

    example: can delete comments

--- examples/posts/components/comment.tsx
+++ examples/posts/components/comment.tsx
@@ -1,11 +1,29 @@
+'use client';
+
+import { useContext } from 'react';
 import Image from 'next/image';
-import { getPost } from '@/lib/prisma/api';
 import type { Prisma } from '@prisma/client';
+import { TrashIcon } from '@heroicons/react/24/solid';
+import { getPost } from '@/lib/prisma/api';
 import { DEFAULT_AVATAR_URL } from '@/lib/image';
+import { UserContext } from '@/context/user';
 
 type postWithComments = Prisma.PromiseReturnType<typeof getPost>;
 
 export default function Comment({ comment }: { comment: postWithComments['comments'][number] }) {
+	const user = useContext(UserContext);
+
+	async function deleteComment() {
+		const response = await fetch(`/api/comments?id=${comment.id}`, { method: 'DELETE' });
+	
+		if (!response.ok) {
+			throw new Error(`DELETE /api/comments: ${response.status} ${JSON.stringify(await response.json())}`);
+		}
+
+		const data = await response.json();
+		console.log(data);
+	}
+
 	return (
 		<div className="bg-white/30 px-4 pb-4 mb-2 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
 			<div className="flex flex-col">
@@ -21,6 +39,7 @@ export default function Comment({ comment }: { comment: postWithComments['commen
 					</div>
 					<p className="text-sm font-semibold">{comment.author.username}</p>
 					<p className="ml-auto text-sm text-gray-500">TODO date</p>
+					{comment.authorId === user?.id && <TrashIcon className="ml-4 h-6 w-6 text-red-300 hover:text-red-500 hover:cursor-pointer" onClick={() => deleteComment()} />}
 				</div>
 				<div className="space-y-1">
 					<p className="font-normal leading-none">{comment.content}</p>
