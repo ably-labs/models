b13865d24da1be8b34958b39d90feaceec0bd0cf
Mike Christensen <mike.christensen@ably.com>
2023-07-13 10:52:13 +0100

    example: move delete comment to dynamic route

--- examples/posts/app/api/comments/[id]/route.ts
+++ examples/posts/app/api/comments/[id]/route.ts
@@ -20,33 +20,35 @@ async function deleteComment(id: number): Promise<Comment> {
 }
 
 export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-  try {
-    let comment: { content: string };
-    let id: number;
-    try {
-      comment = await request.json();
-      id = Number(params.id);
-    } catch (error) {
-      return NextResponse.json({ message: 'failed to read json request body', error }, { status: 400 });
-    }
-    const data = await updateComment(id, comment.content);
-    return NextResponse.json({ data });
-  } catch (error) {
-    return NextResponse.json({ message: 'failed to update comment', error }, { status: 500 });
-  }
+	try {
+		let comment: { content: string };
+		let id: number;
+		try {
+			comment = await request.json();
+			id = Number(params.id);
+		} catch (error) {
+			return NextResponse.json({ message: 'failed to read json request body', error }, { status: 400 });
+		}
+		const data = await updateComment(id, comment.content);
+		return NextResponse.json({ data });
+	} catch (error) {
+		console.error('failed to update comment', error);
+		return NextResponse.json({ message: 'failed to update comment', error }, { status: 500 });
+	}
 }
 
 export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-  try {
-    let id: number;
-    try {
-      id = Number(params.id);
-    } catch (error) {
-      return NextResponse.json({ message: 'failed to read :id url parameter', error }, { status: 400 });
-    }
-    const data = await deleteComment(id);
-    return NextResponse.json({ data });
-  } catch (error) {
-    return NextResponse.json({ message: 'failed to delete comment', error }, { status: 500 });
-  }
+	try {
+		let id: number;
+		try {
+			id = Number(params.id);
+		} catch (error) {
+			return NextResponse.json({ message: 'failed to read :id url parameter', error }, { status: 400 });
+		}
+		const data = await deleteComment(id);
+		return NextResponse.json({ data });
+	} catch (error) {
+		console.error('failed to delete comment', error);
+		return NextResponse.json({ message: 'failed to delete comment', error }, { status: 500 });
+	}
 }
