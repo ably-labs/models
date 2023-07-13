e77d9b35976b4e8d6cfdaee4b8885948122fe1fb
Mike Christensen <mike.christensen@ably.com>
2023-07-13 12:15:03 +0100

    example: move db operations to api file

--- examples/posts/app/api/comments/route.ts
+++ examples/posts/app/api/comments/route.ts
@@ -1,30 +1,15 @@
-import { NextRequest, NextResponse } from 'next/server';
-import type { Comment } from '@prisma/client';
-import prisma from '@/lib/prisma';
-
-async function addComment(req: AddCommentRequest): Promise<Comment> {
-	const comment = await prisma.comment.create({
-		data: req,
-		include: { author: true },
-	});
-	return comment;
-}
-
-type AddCommentRequest = {
-  postId: number;
-  authorId: number;
-  content: string;
-};
+import { NextRequest, NextResponse } from 'next/server'
+import { addComment } from '@/lib/prisma/api';
 
 export async function POST(request: NextRequest) {
 	try {
-		let comment: AddCommentRequest;
+		let comment: { postId: number, authorId: number, content: string };
 			try {
 				comment = await request.json();
 			} catch (error) {
 				return NextResponse.json({ message: 'failed to read json request body', error }, { status: 400 });
 			}
-			const data = await addComment(comment);
+			const data = await addComment(comment.postId, comment.authorId, comment.content);
 			return NextResponse.json({ data });
 	} catch (error) {
 		console.error('failed to add comment', error);
