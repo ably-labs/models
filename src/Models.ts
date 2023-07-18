0a4b4ce079413d45c3e72343d26c5b03677423d8
Mike Christensen <mike.christensen@ably.com>
2023-07-18 11:08:33 +0100

    models: simplify library instantiation
    The models constructor now requires an ably instance to be provided
    explicitly and no longer supports passing in ably client config.
    
    Additionally, this top level instatiates the logger which is passed to
    the model and shared through the application.

--- src/Models.ts
+++ src/Models.ts
@@ -1,77 +1,36 @@
-import Ably, { Types } from 'ably';
-import type { LevelWithSilent } from 'pino';
+import type { Types } from 'ably/promises';
 import Model, { ModelOptions } from './Model.js';
-import Stream, { StreamOptions } from './Stream.js';
+import pino, { LevelWithSilent, Logger } from 'pino';
 
 export type ModelsOptions = {
+  ably: Types.RealtimePromise;
   logLevel?: LevelWithSilent;
-};
-
-const MODELS_OPTIONS_DEFAULTS: ModelsOptions = {
-  logLevel: 'silent',
 };
 
 class Models {
   public ably: Types.RealtimePromise;
+  private logger: Logger;
   private models: Record<string, Model<any>>;
-  private streams: Record<string, Stream>;
-  private options: ModelsOptions;
 
   readonly version = '0.0.1';
 
-  constructor(optionsOrAbly: Types.RealtimePromise | Types.ClientOptions | string, options?: ModelsOptions) {
+  constructor(options: ModelsOptions) {
     this.models = {};
-    this.streams = {};
-    if (optionsOrAbly['options']) {
-      this.ably = optionsOrAbly as Types.RealtimePromise;
-      this.addAgent(this.ably['options'], false);
-    } else {
-      let options: Types.ClientOptions = typeof optionsOrAbly === 'string' ? { key: optionsOrAbly } : optionsOrAbly;
-      this.addAgent(options, true);
-      this.ably = new Ably.Realtime.Promise(options);
-    }
+    this.ably = options.ably;
+    this.logger = pino({ level: options.logLevel || 'silent' });
     this.ably.time();
-    this.options = { ...MODELS_OPTIONS_DEFAULTS, ...options };
   }
 
-  private addAgent(options: any, isDefault: boolean) {
-    const agent = `ably-models/${this.version}`;
-    const clientType = isDefault ? 'model-default-client' : 'model-custom-client';
-    if (!options.agents) {
-      options.agents = [agent, clientType];
-    } else if (!options.agents.includes(agent)) {
-      options.agents.push(agent, clientType);
-    }
-  }
-
-  Model = <T>(name: string, options: ModelOptions<T>) => {
+  Model = <T>(name: string, options: Omit<ModelOptions<T>, 'ably' | 'logger'>) => {
     if (typeof name !== 'string' || name.length === 0) {
       throw new Error('Model must have a non-empty name');
     }
-
-    if (this.models[name]) return this.models[name];
-
-    const model = new Model<T>(name, { logLevel: this.options.logLevel, ...options });
+    if (this.models[name]) {
+      return this.models[name];
+    }
+    const model = new Model<T>(name, { ably: this.ably, logger: this.logger, ...options });
     this.models[name] = model;
-
     return model;
-  };
-
-  Stream = (name: string, options?: StreamOptions) => {
-    if (typeof name !== 'string' || name.length === 0) {
-      throw new Error('Stream must have a non-empty name');
-    }
-
-    if (this.streams[name]) return this.streams[name];
-
-    if (!options) {
-      throw new Error('Stream cannot be instantiated without options');
-    }
-
-    const stream = new Stream(this.ably, { logLevel: this.options.logLevel, ...options });
-    this.streams[name] = stream;
-
-    return stream;
   };
 }
 
