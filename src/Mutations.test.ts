099d8847447ab70b2f4daa6976eacc1ce77bded5
Mike Christensen <mike.christensen@ably.com>
2023-07-20 16:37:38 +0100

    mutations: updates tests
    - Aligns tests to use separate update and confirmation promises
    - Adds test to check apply updates failure

--- src/Mutations.test.ts
+++ src/Mutations.test.ts
@@ -1,5 +1,5 @@
 import { it, describe, expect, vi } from 'vitest';
-import Mutations, { MutationMethods, MutationFunc } from './Mutations.js';
+import Mutations, { MutationMethods, MutationFunc, DEFAULT_OPTIONS } from './Mutations.js';
 import type { Event } from './Model.js';
 
 interface Methods extends MutationMethods {
@@ -11,7 +11,7 @@ interface MutationsTestContext {}
 
 describe('Mutations', () => {
   it<MutationsTestContext>('invokes mutation methods', async () => {
-    let onEvents = vi.fn(() => Promise.resolve());
+    let onEvents = vi.fn(() => [Promise.resolve(), Promise.resolve()]);
     let onError = vi.fn();
     const mutations = new Mutations<Methods>({ onEvents, onError });
     mutations.register({
@@ -28,7 +28,7 @@ describe('Mutations', () => {
   });
 
   it<MutationsTestContext>('mutation method throws and calls onError', async () => {
-    let onEvents = vi.fn(() => Promise.resolve());
+    let onEvents = vi.fn(() => [Promise.resolve(), Promise.resolve()]);
     let onError = vi.fn();
     const mutations = new Mutations<Pick<Methods, 'one'>>({ onEvents, onError });
     mutations.register({
@@ -44,7 +44,7 @@ describe('Mutations', () => {
   });
 
   it<MutationsTestContext>('invokes mutation methods with expectations and options', async () => {
-    let onEvents = vi.fn(() => Promise.resolve());
+    let onEvents = vi.fn(() => [Promise.resolve(), Promise.resolve()]);
     let onError = vi.fn();
     const mutations = new Mutations<Methods>({ onEvents, onError });
     mutations.register({
@@ -56,23 +56,25 @@ describe('Mutations', () => {
     });
 
     const events: Event[] = [{ channel: 'channel', name: 'foo', data: { bar: 123 } }];
-    const [result1, confirmed1] = await mutations.handler.one.$expect(events)('foo');
-    expect(result1).toEqual('foo');
-    await expect(confirmed1).resolves.toBeUndefined();
+    const result1 = await mutations.handler.one.$expect(events)('foo');
+    expect(result1[0]).toEqual('foo');
+    await expect(result1[1]).resolves.toBeUndefined();
+    await expect(result1[2]).resolves.toBeUndefined();
     expect(onEvents).toHaveBeenCalledTimes(1);
-    expect(onEvents).toHaveBeenNthCalledWith(1, events, undefined);
+    expect(onEvents).toHaveBeenNthCalledWith(1, events, DEFAULT_OPTIONS);
     expect(onError).not.toHaveBeenCalled();
 
-    const [result2, confirmed2] = await mutations.handler.two.$expect(events)(123);
-    expect(result2).toEqual({ x: 123 });
-    await expect(confirmed2).resolves.toBeUndefined();
+    const result2 = await mutations.handler.two.$expect(events)(123);
+    expect(result2[0]).toEqual({ x: 123 });
+    await expect(result2[1]).resolves.toBeUndefined();
+    await expect(result2[2]).resolves.toBeUndefined();
     expect(onEvents).toHaveBeenCalledTimes(2);
     expect(onEvents).toHaveBeenNthCalledWith(2, events, { timeout: 1000 });
     expect(onError).not.toHaveBeenCalled();
   });
 
   it<MutationsTestContext>('mutation methods with expectations and options throw and call onError', async () => {
-    let onEvents = vi.fn(() => Promise.resolve());
+    let onEvents = vi.fn(() => [Promise.resolve(), Promise.resolve()]);
     let onError = vi.fn();
     const mutations = new Mutations<Methods>({ onEvents, onError });
     mutations.register({
@@ -89,20 +91,18 @@ describe('Mutations', () => {
 
     const events: Event[] = [{ channel: 'channel', name: 'foo', data: { bar: 123 } }];
     await expect(mutations.handler.one.$expect(events)('foo')).rejects.toEqual('foo');
-    expect(onEvents).toHaveBeenCalledTimes(1);
-    expect(onEvents).toHaveBeenNthCalledWith(1, events, undefined);
+    expect(onEvents).toHaveBeenCalledTimes(0);
     expect(onError).toHaveBeenCalledTimes(1);
     expect(onError).toHaveBeenNthCalledWith(1, 'foo', events);
 
     await expect(mutations.handler.two.$expect(events)(123)).rejects.toEqual(123);
-    expect(onEvents).toHaveBeenCalledTimes(2);
-    expect(onEvents).toHaveBeenNthCalledWith(2, events, { timeout: 1000 });
+    expect(onEvents).toHaveBeenCalledTimes(0);
     expect(onError).toHaveBeenCalledTimes(2);
     expect(onError).toHaveBeenNthCalledWith(2, 123, events);
   });
 
-  it<MutationsTestContext>('mutation methods with expectations and options are not confirmed', async () => {
-    let onEvents = vi.fn(() => Promise.reject());
+  it<MutationsTestContext>('apply updates fails', async () => {
+    let onEvents = vi.fn(() => [Promise.reject(), Promise.resolve()]);
     let onError = vi.fn();
     const mutations = new Mutations<Methods>({ onEvents, onError });
     mutations.register({
@@ -114,16 +114,48 @@ describe('Mutations', () => {
     });
 
     const events: Event[] = [{ channel: 'channel', name: 'foo', data: { bar: 123 } }];
-    const [result1, confirmed1] = await mutations.handler.one.$expect(events)('foo');
-    expect(result1).toEqual('foo');
-    await expect(confirmed1).rejects.toBeUndefined();
+    const result1 = await mutations.handler.one.$expect(events)('foo');
+    expect(result1[0]).toEqual('foo');
+    await expect(result1[1]).rejects.toBeUndefined();
+    await expect(result1[2]).resolves.toBeUndefined();
     expect(onEvents).toHaveBeenCalledTimes(1);
-    expect(onEvents).toHaveBeenNthCalledWith(1, events, undefined);
+    expect(onEvents).toHaveBeenNthCalledWith(1, events, DEFAULT_OPTIONS);
     expect(onError).not.toHaveBeenCalled();
 
-    const [result2, confirmed2] = await mutations.handler.two.$expect(events)(123);
-    expect(result2).toEqual({ x: 123 });
-    await expect(confirmed2).rejects.toBeUndefined();
+    const result2 = await mutations.handler.two.$expect(events)(123);
+    expect(result2[0]).toEqual({ x: 123 });
+    await expect(result2[1]).rejects.toBeUndefined();
+    await expect(result2[2]).resolves.toBeUndefined();
+    expect(onEvents).toHaveBeenCalledTimes(2);
+    expect(onEvents).toHaveBeenNthCalledWith(2, events, { timeout: 1000 });
+    expect(onError).not.toHaveBeenCalled();
+  });
+
+  it<MutationsTestContext>('methods not confirmed', async () => {
+    let onEvents = vi.fn(() => [Promise.resolve(), Promise.reject()]);
+    let onError = vi.fn();
+    const mutations = new Mutations<Methods>({ onEvents, onError });
+    mutations.register({
+      one: async (x: string) => x,
+      two: {
+        func: async (x: number) => ({ x }),
+        options: { timeout: 1000 },
+      },
+    });
+
+    const events: Event[] = [{ channel: 'channel', name: 'foo', data: { bar: 123 } }];
+    const result1 = await mutations.handler.one.$expect(events)('foo');
+    expect(result1[0]).toEqual('foo');
+    await expect(result1[1]).resolves.toBeUndefined();
+    await expect(result1[2]).rejects.toBeUndefined();
+    expect(onEvents).toHaveBeenCalledTimes(1);
+    expect(onEvents).toHaveBeenNthCalledWith(1, events, DEFAULT_OPTIONS);
+    expect(onError).not.toHaveBeenCalled();
+
+    const result2 = await mutations.handler.two.$expect(events)(123);
+    expect(result2[0]).toEqual({ x: 123 });
+    await expect(result2[1]).resolves.toBeUndefined();
+    await expect(result2[2]).rejects.toBeUndefined();
     expect(onEvents).toHaveBeenCalledTimes(2);
     expect(onEvents).toHaveBeenNthCalledWith(2, events, { timeout: 1000 });
     expect(onError).not.toHaveBeenCalled();
