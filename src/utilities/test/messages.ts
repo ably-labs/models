3492dc12445cbf5c9a7621b4f66667b9810ee6e0
Mike Christensen <mike.christensen@ably.com>
2023-08-07 17:51:57 +0100

    models: remove mock server implementation

--- src/utilities/test/messages.ts
+++ src/utilities/test/messages.ts
@@ -1,26 +1,4 @@
 import { Types } from 'ably/promises';
-import { Flags } from './protocol.js';
-
-export const baseProtocolMessage = {
-  flags: Flags.PRESENCE | Flags.PUBLISH | Flags.SUBSCRIBE | Flags.PRESENCE_SUBSCRIBE,
-  id: 'PROTOCOL_MESSAGE_ID',
-  timestamp: 1,
-  count: 1,
-  connectionId: 'CONNECTION_ID',
-  channel: 'foobar',
-  channelSerial: 'CHANNEL_SERIAL',
-  msgSerial: 1,
-  connectionDetails: {
-    clientId: '',
-    connectionKey: 'randomKey',
-    maxMessageSize: 131000,
-    maxInboundRate: 1000,
-    maxOutboundRate: 1000,
-    maxFrameSize: 262144,
-    connectionStateTtl: 120000,
-    maxIdleInterval: 15000,
-  },
-};
 
 export const baseMessage: Types.Message = {
   id: '1',
