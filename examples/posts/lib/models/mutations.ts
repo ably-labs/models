826ff820d4162489b6aa1da36e1e8f784b27e2fa
Mike Christensen <mike.christensen@ably.com>
2023-08-08 17:27:51 +0100

    demo: emit optimistic event before api response

--- examples/posts/lib/models/mutations.ts
+++ examples/posts/lib/models/mutations.ts
@@ -1,35 +1,41 @@
-import type { Comment as CommentType, Author as AuthorType } from '@/lib/prisma/api';
+import type { Author as AuthorType } from '@/lib/prisma/api';
 
 export async function addComment(author: AuthorType, postId: number, content: string) {
-  const response = await fetch('/api/comments', {
+  fetch('/api/comments', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ authorId: author.id, postId, content }),
-  });
-  if (!response.ok) {
-    throw new Error(`POST /api/comments: ${response.status} ${JSON.stringify(await response.json())}`);
-  }
-  return (await response.json()) as { data: CommentType };
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        throw new Error(`POST /api/comments: ${response.status} ${JSON.stringify(await response.json())}`);
+      }
+    })
+    .catch((err) => console.error('addComment:', err));
 }
 
 export async function editComment(id: number, content: string) {
-  const response = await fetch(`/api/comments/${id}`, {
+  fetch(`/api/comments/${id}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ content }),
-  });
-  if (!response.ok) {
-    throw new Error(`PUT /api/comments/:id: ${response.status} ${JSON.stringify(await response.json())}`);
-  }
-  return (await response.json()) as { data: CommentType };
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        throw new Error(`PUT /api/comments/:id: ${response.status} ${JSON.stringify(await response.json())}`);
+      }
+    })
+    .catch((err) => console.error('editComment:', err));
 }
 
 export async function deleteComment(id: number) {
-  const response = await fetch(`/api/comments/${id}`, { method: 'DELETE' });
-  if (!response.ok) {
-    throw new Error(`DELETE /api/comments/:id: ${response.status} ${JSON.stringify(await response.json())}`);
-  }
-  return await response.json();
+  fetch(`/api/comments/${id}`, { method: 'DELETE' })
+    .then(async (response) => {
+      if (!response.ok) {
+        throw new Error(`DELETE /api/comments/:id: ${response.status} ${JSON.stringify(await response.json())}`);
+      }
+    })
+    .catch((err) => console.error('deleteComment:', err));
 }
 
 export type Mutations = {
