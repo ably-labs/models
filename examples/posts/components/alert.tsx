8655e0c8adf0cf380f57c289b9993a3e6ad2f4db
Mike Christensen <mike.christensen@ably.com>
2023-08-08 16:52:02 +0100

    demo: add alerts to demonstrate library behaviour

---
+++ examples/posts/components/alert.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useAlert, Alert } from '@/context/alert';
+
+const typeToColour = (type: Alert['type']): string => {
+  if (type === 'success') {
+    return 'bg-green-500';
+  }
+  if (type === 'info') {
+    return 'bg-blue-500';
+  }
+  if (type === 'error') {
+    return 'bg-red-500';
+  }
+  return 'bg-gray-500';
+}
+
+const Alert = ({ alert }: { alert: Alert }) => (
+  <div className={`${typeToColour(alert.type)} text-white px-4 py-2 rounded mt-4 shadow`}>
+    {alert.message}
+  </div>
+);
+
+const AlertContainer = () => {
+  const { alerts } = useAlert();
+  return (
+    <div className="fixed bottom-0 left-0 z-50 flex flex-col items-start space-y-4 p-4">
+      {alerts.map((alert) => (
+        <Alert key={alert.id} alert={alert} />
+      ))}
+    </div>
+  );
+};
+
+export default AlertContainer;
--- examples/posts/components/post.tsx
+++ examples/posts/components/post.tsx
@@ -2,13 +2,13 @@
 
 import { useEffect, useState } from 'react';
 import type { Event } from '@ably-labs/models';
-import { configureAbly } from '@ably-labs/react-hooks';
 import type { Post as PostType, Author as AuthorType } from '@/lib/prisma/api';
 import { AuthorProvider } from '@/context/author';
+import { AlertProvider, useAlert } from '@/context/alert';
 import Comments from '@/components/comments';
-import { usePost } from '@/lib/models/hook';
-
-configureAbly({ key: process.env.NEXT_PUBLIC_ABLY_API_KEY });
+import PostPlaceholder from '@/components/post-placeholder';
+import AlertContainer from '@/components/alert';
+import { useModel, type ModelType } from '@/lib/models/hook';
 
 // compare optimistic and confirmed comment mutation events by content,
 // ignoring other attributes not available on the optimistic event.
@@ -20,9 +20,9 @@ function compareComments(optimistic: Event, confirmed: Event) {
   );
 }
 
-export default function Post({ user, post: initialPost }: { user: AuthorType, post: PostType }) {
-  const [post, setPost] = useState<PostType>(initialPost);
-  const model = usePost(initialPost.id);
+function Post({ model }: { model: ModelType }) {
+  const { setAlert } = useAlert();
+  const [post, setPost] = useState<PostType>(model.confirmed);
 
   useEffect(() => {
     const onUpdate = (err: Error, post: PostType) => {
@@ -39,63 +39,85 @@ export default function Post({ user, post: initialPost }: { user: AuthorType, po
       model.unsubscribe(onUpdate);
     };
   });
-
+  
   async function onAdd(author: AuthorType, postId: number, content: string) {
     if (!model) return;
-    const [result, update, confirmation] = await model.mutations.addComment.$expect(
+    const [, update, confirmation] = await model.mutations.addComment.$expect(
       [{ channel: 'comments', name: 'add', data: { author, content } }],
       compareComments,
     )(author, postId, content);
-    console.log('onAdd:', result);
-    await update;
-    console.log('onAdd: optimistically updated');
-    await confirmation;
-    console.log('onAdd: confirmed');
+    try {
+      await update;
+      setAlert('Optimistically adding comment', 'info');
+      await confirmation;
+      setAlert('Add comment confirmed!', 'success');
+    } catch (err) {
+      setAlert(`Error adding comment: ${err}`, 'error')
+    }
   }
   
   async function onEdit(id: number, content: string) {
     if (!model) return;
-    const [result, update, confirmation] = await model.mutations.editComment.$expect(
+    const [, update, confirmation] = await model.mutations.editComment.$expect(
       [{ channel: 'comments', name: 'edit', data: { id, content } }],
       compareComments,
     )(id, content);
-    console.log('onEdit:', result);
-    await update;
-    console.log('onEdit: optimistically updated');
-    await confirmation;
-    console.log('onEdit: confirmed');
+    try {
+      await update;
+      setAlert('Optimistically edited comment', 'info');
+      await confirmation;
+      setAlert('Edit comment confirmed!', 'success');
+    } catch (err) {
+      setAlert(`Error editing comment: ${err}`, 'error')
+    }
   }
   
   async function onDelete(id: number) {
     if (!model) return;
-    const [result, update, confirmation] = await model.mutations.deleteComment.$expect(
+    const [, update, confirmation] = await model.mutations.deleteComment.$expect(
       [{ channel: 'comments', name: 'delete', data: { id } }],
       compareComments,
     )(id);
-    console.log('onDelete:', result);
-    await update;
-    console.log('onDelete: optimistically updated');
-    await confirmation;
-    console.log('onDelete: confirmed');
+    try {
+      await update;
+      setAlert('Optimistically deleted comment', 'info');
+      await confirmation;
+      setAlert('Delete comment confirmed!', 'success');
+    } catch (err) {
+      setAlert(`Error deleting comment: ${err}`, 'error')
+    }
   }
 
   return (
-    <AuthorProvider author={user}>
-      <main className="relative flex min-h-screen flex-col items-center justify-center">
-        <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-xl font-medium tracking-tight text-transparent md:text-4xl">
-          {post.title}
-        </h1>
-        <div className="space-y-1 mb-8">
-          <p className="font-normal text-gray-500 leading-none">{post.content}</p>
-        </div>
-        <Comments
-          postId={post.id}
-          comments={post.comments}
-          onEdit={onEdit}
-          onAdd={onAdd}
-          onDelete={onDelete}
-        ></Comments>
-      </main>
-    </AuthorProvider>
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-xl font-medium tracking-tight text-transparent md:text-4xl">
+        {post.title}
+      </h1>
+      <div className="space-y-1 mb-8">
+        <p className="font-normal text-gray-500 leading-none">{post.content}</p>
+      </div>
+      <Comments
+        postId={post.id}
+        comments={post.comments}
+        onEdit={onEdit}
+        onAdd={onAdd}
+        onDelete={onDelete}
+      ></Comments>
+    </main>
   );
+}
+
+export default function PostWrapper({ user, post: initialPost }: { user: AuthorType, post: PostType }) {
+  const model = useModel(initialPost.id);
+  if (!model) {
+    return <PostPlaceholder />;
+  }
+  return (
+    <AuthorProvider author={user}>
+      <AlertProvider>
+        <AlertContainer />
+        <Post model={model} />
+      </AlertProvider>
+    </AuthorProvider>
+  )
 }
