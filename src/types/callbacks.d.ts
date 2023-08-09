7b27ac2951afb5f8e5059a372119347c978b6182
Mike Christensen <mike.christensen@ably.com>
2023-08-09 10:55:58 +0100

    model: fix subscribe callback type inference
    - Updates the StandardCallback type definition to use an overload
    - subscribe/unsubscribe callback type is directly defined so that TypeScript can easily infer the type of the callback, which improves the developer experience

--- src/types/callbacks.d.ts
+++ src/types/callbacks.d.ts
@@ -1,2 +1,4 @@
-export type StandardCallback<T> = (err?: Error | null, result?: T) => void;
-export type ErrCallback = (err?: Error | null) => void;
+export type StandardCallback<T> = {
+  (err: Error): void;
+  (err: null, result: T): void;
+};
