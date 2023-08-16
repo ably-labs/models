c20b8380889d00b4a52540cdf4abaab86f5adb54
Mike Christensen <mike.christensen@ably.com>
2023-08-16 17:35:43 +0100

    test: add pending confirmations registry tests

--- src/utilities/test/events.ts
+++ src/utilities/test/events.ts
@@ -1,9 +1,23 @@
-import type { Event, EventParams, OptimisticEventWithParams } from '../../types/model';
+import type { ConfirmedEvent, Event, EventParams, OptimisticEvent, OptimisticEventWithParams } from '../../types/model';
 
-export function toExpectedEvents(events: Event[], params: EventParams): OptimisticEventWithParams[] {
+export function toOptimisticEventsWithParams(events: Event[], params: EventParams): OptimisticEventWithParams[] {
   return events.map((event) => ({
     ...event,
     confirmed: false,
     params,
+  }));
+}
+
+export function toOptimisticEvents(events: Event[]): OptimisticEvent[] {
+  return events.map((event) => ({
+    ...event,
+    confirmed: false,
+  }));
+}
+
+export function toConfirmedEvents(events: Event[]): ConfirmedEvent[] {
+  return events.map((event) => ({
+    ...event,
+    confirmed: true,
   }));
 }
