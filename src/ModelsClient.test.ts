78ff254bb39e136eac24d478b168dea9dc2ce299
zak <zak.knill@ably.com>
2023-10-19 10:05:41 +0100

    Add models namespace and constructor on client
    Update Models to be called ModelsClient, the client gives access to
    models using:
    
    ```
    modelsClient.models.get<T>({...})
    ```
    
    The get function accepts the model name, channel name, and registration
    arguments ($sync and $merge functions) in order to build and register
    the model.

--- src/ModelsClient.test.ts
+++ src/ModelsClient.test.ts
@@ -1,7 +1,7 @@
 import { Realtime, Types } from 'ably/promises';
 import { vi, it, describe, expect, expectTypeOf, beforeEach } from 'vitest';
 
-import Models from './Models.js';
+import ModelsClient from './ModelsClient.js';
 
 interface ModelsTestContext {
   ably: Types.RealtimePromise;
@@ -24,15 +24,26 @@ describe('Models', () => {
   });
 
   it<ModelsTestContext>('expects the injected client to be of the type RealtimePromise', ({ ably }) => {
-    const models = new Models({ ably });
-    expectTypeOf(models.ably).toMatchTypeOf<Types.RealtimePromise>();
+    const modelsClient = new ModelsClient({ ably });
+    expectTypeOf(modelsClient.ably).toMatchTypeOf<Types.RealtimePromise>();
   });
 
   it<ModelsTestContext>('getting a model with the same name returns the same instance', ({ ably, channelName }) => {
-    const models = new Models({ ably });
-    const model1 = models.Model<string>('test', channelName);
+    const modelsClient = new ModelsClient({ ably });
+    const model1 = modelsClient.models.get<string>({
+      name: 'test',
+      channelName: channelName,
+      $sync: async () => 'initial data',
+      $merge: async () => 'merged',
+    });
     expect(model1.name).toEqual('test');
-    const model2 = models.Model<string>('test', channelName);
+
+    const model2 = modelsClient.models.get<string>({
+      name: 'test',
+      channelName: channelName,
+      $sync: async () => 'initial data',
+      $merge: async () => 'merged',
+    });
     expect(model2.name).toEqual('test');
     expect(model1).toEqual(model2);
   });
