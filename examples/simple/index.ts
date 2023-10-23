c0e405e1ebd93f3c2018426b9ab2be2a341cc7dd
zak <zak.knill@ably.com>
2023-10-23 17:33:52 +0100

    Update simple example app for new models api

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,6 +1,7 @@
-import Models, { type MutationContext } from '@ably-labs/models';
+import ModelsClient, {Event, ConfirmedEvent, OptimisticEvent} from '@ably-labs/models';
 import Ably from 'ably/promises';
 import pino from 'pino';
+import {v4 as uuidv4} from 'uuid';
 
 const logger = pino();
 
@@ -10,93 +11,77 @@ type Post = {
   comments: string[];
 };
 
-type Mutations = {
-  updatePost: (context: MutationContext, text: string) => Promise<{ status: number }>;
-  addComment: (context: MutationContext, text: string) => Promise<{ status: number }>;
-};
-
 const ably = new Ably.Realtime.Promise({
   key: process.env.ABLY_API_KEY,
 });
-const models = new Models({ ably, logLevel: 'silent' });
+
+const modelsClient = new ModelsClient({ably, logLevel: 'silent', optimisticEventOptions: {timeout: 5000}});
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 (async function main() {
-  const model = models.Model<Post, Mutations>('post');
+  const channelName = 'models:post:' + uuidv4();
 
   async function sync() {
-    return {
+    const data: Post = {
       id: 123,
       text: 'initial state',
       comments: [],
     };
+    const sequenceID = '10000';
+    return {data, sequenceID};
   }
 
-  await model.$register({
-    $sync: sync,
-    $update: {
-      'posts:123': {
-        update: async (state, event) => ({
+  let sequentialID = 1;
+
+  async function simulateConfirmation(event: Event) {
+    setTimeout(async () => {
+      const message = {
+        id: '' + sequentialID++,
+        name: event.name,
+        data: event.data,
+        extras: {
+          headers: {
+            'x-ably-models-event-uuid': event.mutationID,
+          },
+        },
+      };
+      await ably.channels.get(channelName).publish(message);
+      logger.debug({message}, 'simulated confirmation');
+    }, 3000);
+  }
+
+  async function merge(state: Post, event: OptimisticEvent | ConfirmedEvent) {
+    logger.info({event}, 'merging event');
+
+    switch (event.name) {
+      case 'updatePost':
+        return {
           ...state,
           text: event.data,
-        }),
-      },
-      'posts:123:comments': {
-        add: async (state, event) => ({
+        };
+      case 'addComment':
+        return {
           ...state,
           comments: state.comments.concat([event.data]),
-        }),
-      },
-    },
-    $mutate: {
-      updatePost: async (context: MutationContext) => {
-        // simulate confirmation
-        setTimeout(async () => {
-          for (const event of context.events) {
-            await ably.channels.get('posts:123').publish({
-              name: 'update',
-              data: postText,
-              extras: {
-                headers: {
-                  'x-ably-models-event-uuid': event.uuid,
-                },
-              },
-            });
-          }
-        }, 5000);
-        return { status: 200 };
-      },
-      addComment: async (context: MutationContext) => {
-        // simulate confirmation
-        setTimeout(async () => {
-          for (const event of context.events) {
-            await ably.channels.get('posts:123:comments').publish({
-              name: 'add',
-              data: commentText,
-              extras: {
-                headers: {
-                  'x-ably-models-event-uuid': event.uuid,
-                },
-              },
-            });
-          }
-        }, 5000);
-        return { status: 200 };
-      },
-    },
-  });
+        };
+    }
+  }
 
-  model.on((event) => logger.info({ event }, 'model state update'));
+  const model = modelsClient.models.get<Post>({name: 'post', channelName: channelName, sync: sync, merge: merge});
+  logger.info('sync: starting the model');
+  await model.sync();
+
+  model.on((event) => logger.info({event}, 'model state update'));
 
   model.subscribe(
     (err, post) => {
       if (err) {
         throw err;
       }
-      logger.info({ post }, 'subscribe (non-optimistic)');
+      logger.info({post}, 'subscribe (non-optimistic)');
     },
-    { optimistic: false },
+    {optimistic: false},
   );
 
   model.subscribe(
@@ -104,45 +89,40 @@ const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
       if (err) {
         throw err;
       }
-      logger.info({ post }, 'subscribe (optimistic)');
+      logger.info({post}, 'subscribe (optimistic)');
     },
-    { optimistic: true },
+    {optimistic: true},
   );
 
-  const postText = 'update post';
-  const [postResult, postConfirmation] = await model.mutations.updatePost.$expect({
-    events: [
-      {
-        channel: 'posts:123',
-        name: 'update',
-        data: postText,
-      },
-    ],
-  })(postText);
-  logger.info(postResult, 'mutation: updatePost');
+  const event1: Event = {
+    mutationID: uuidv4(),
+    name: 'updatePost',
+    data: 'my updated post',
+  };
 
+  const [confirmedUpdate] = await model.optimistic(event1);
+  logger.info('mutation: updatePost');
   logger.info('mutation: updatePost: awaiting confirmation...');
-  await postConfirmation;
+  simulateConfirmation(event1);
+
+  await confirmedUpdate;
   logger.info('mutation: updatePost: confirmed');
 
-  const commentText = 'add comment';
-  const [commentResult, commentConfirmation] = await model.mutations.addComment.$expect({
-    events: [
-      {
-        channel: 'posts:123:comments',
-        name: 'add',
-        data: commentText,
-      },
-    ],
-  })(commentText);
-  logger.info(commentResult, 'mutation: addComment');
-
+  const event2 = {
+    mutationID: uuidv4(),
+    name: 'addComment',
+    data: 'my new comment',
+  };
+  const [confirmedAdd] = await model.optimistic(event2);
+  logger.info('mutation: addComment');
   logger.info('mutation: addComment: awaiting confirmation...');
-  await commentConfirmation;
+  simulateConfirmation(event2);
+
+  await confirmedAdd;
   logger.info('mutation: addComment: confirmed');
 
   await wait(1000);
-  await model.$dispose();
+  await model.dispose();
   ably.close();
   logger.info('goodbye');
 })();
