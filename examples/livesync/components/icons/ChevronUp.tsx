a7067ca9b7b627b14a0e492dec1c2e8bea808078
snikidev <n.kakuev@gmail.com>
2023-11-22 13:42:40 +0000

    Implement layout

---
+++ examples/livesync/components/icons/ChevronUp.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from 'react';
+
+export const ChevronUpIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M15 12.5L10 7.5L5 12.5"
+        stroke="currentColor"
+        strokeWidth="1.38889"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
