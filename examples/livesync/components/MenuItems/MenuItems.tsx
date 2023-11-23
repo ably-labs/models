69d3c275c75ffdd8e534a286fa54ead7d099a42b
Mike Christensen <mikescottchristensen@gmail.com>
2023-11-23 15:07:52 +0000

    Revert "Implement project list view"
    This reverts commit 67b32da9ff45bd6c03cbda8c1402b91869ea6665.

---
+++ examples/livesync/components/MenuItems/MenuItems.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import * as Accordion from '@radix-ui/react-accordion';
+import { usePathname } from 'next/navigation';
+
+import { Item, MenuItemType } from './Item';
+
+interface Props {
+  items: MenuItemType[];
+}
+
+export const MenuItems = ({ items }: Props) => {
+  const pathname = usePathname();
+
+  return (
+    <Accordion.Root
+      type="single"
+      collapsible
+      defaultValue={pathname.includes('/projects') ? 'projects' : undefined}
+    >
+      {items.map((item) => (
+        <Item
+          key={item.value}
+          {...item}
+        />
+      ))}
+    </Accordion.Root>
+  );
+};
