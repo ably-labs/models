f1d83681722886b2ee92be51bc1631d87286f754
snikidev <n.kakuev@gmail.com>
2023-11-27 16:38:59 +0000

    Feedback

--- examples/livesync/data/seed.ts
+++ examples/livesync/data/seed.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 import sql from './db';
-import { comments, issues, projects, users } from './fakeData';
+import { createComments, createIssues, projects, users } from './fakeData';
 
 const seedData = async () => {
   await sql.begin(async (sql) => {
@@ -16,91 +16,93 @@ const seedData = async () => {
 
     await sql`
       CREATE TABLE projects (
-        id UUID PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         slug VARCHAR(255) UNIQUE NOT NULL,
         name VARCHAR(255) NOT NULL,
         description TEXT NOT NULL,
-        color VARCHAR(255) NOT NULL
+        color VARCHAR(255) NOT NULL,
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
       )`;
     const projectsRows = await sql`INSERT INTO projects ${sql(
       projects,
-      'id',
       'slug',
       'name',
       'description',
       'color',
-    )} RETURNING *`;
+    )} RETURNING id`;
 
     console.log(chalk.greenBright('🗄️  Created and seeded projects table'));
 
     await sql`
       CREATE TABLE users (
-        id UUID PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         slug VARCHAR(255) UNIQUE NOT NULL,
         first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
         email VARCHAR(255) NOT NULL,
         color VARCHAR(255) NOT NULL,
-        date_added DATE NOT NULL,
-        title VARCHAR(255) NOT NULL
+        title VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
       )`;
     const usersRows = await sql`INSERT INTO users ${sql(
       users,
-      'id',
       'slug',
       'first_name',
       'last_name',
       'email',
       'color',
-      'date_added',
       'title',
-    )} RETURNING *`;
+    )} RETURNING id`;
 
     console.log(chalk.greenBright('💁‍♀️ Created and seeded users table'));
 
     await sql`
       CREATE TABLE issues (
-        id UUID PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         slug VARCHAR(255) UNIQUE NOT NULL,
         name VARCHAR(255) NOT NULL,
-        due_date DATE NOT NULL,
+        due_date TIMESTAMP NOT NULL DEFAULT NOW(),
         status statuses NOT NULL,
-        owner UUID REFERENCES users,
+        owner_id SERIAL REFERENCES users,
         story_points INTEGER NOT NULL,
         description TEXT NOT NULL,
-        project_id UUID NOT NULL REFERENCES projects
+        project_id SERIAL NOT NULL REFERENCES projects,
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        created_at TIMESTAMP NOT NULL DEFAULT NOW()
       )`;
+    const issues = createIssues(usersRows, projectsRows);
     const issuesRows = await sql`INSERT INTO issues ${sql(
       issues,
-      'id',
       'slug',
       'name',
       'due_date',
       'status',
-      'owner',
+      'owner_id',
       'story_points',
       'description',
       'project_id',
-    )} RETURNING *`;
+    )} RETURNING id`;
 
     console.log(chalk.greenBright('🎟️  Created and seeded users table'));
 
     await sql`
       CREATE TABLE comments (
-        id UUID PRIMARY KEY,
-        issue UUID REFERENCES issues,
-        user_id UUID REFERENCES users,
-        created_on DATE NOT NULL,
-        content TEXT NOT NULL
+        id SERIAL PRIMARY KEY,
+        issue_id SERIAL REFERENCES issues,
+        user_id SERIAL REFERENCES users,
+        content TEXT NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+        updated_at TIMESTAMP NOT NULL DEFAULT NOW()
       )`;
+
+    const comments = createComments(issuesRows, usersRows);
     const commentsRows = await sql`INSERT INTO comments ${sql(
       comments,
-      'id',
-      'issue',
+      'issue_id',
       'user_id',
-      'created_on',
       'content',
-    )} RETURNING *`;
+    )} RETURNING id`;
 
     console.log(chalk.greenBright('💬 Created and seeded users table'));
 
