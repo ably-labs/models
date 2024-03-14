1204cfbb6fb6f12048580d5d8feb523b32b9e1e8
Matt Kaschula <matthew.kaschula@ably.com>
2024-03-14 09:45:03 +0000

    format

--- src/Model.test.ts
+++ src/Model.test.ts
@@ -150,7 +150,9 @@ describe('Model', () => {
     );
     model.on('errored', erroredListener);
 
-    await model.sync().catch((err) => expect(err.message).toEqual('The sync function returned an undefined sequenceId'));
+    await model
+      .sync()
+      .catch((err) => expect(err.message).toEqual('The sync function returned an undefined sequenceId'));
     expect(sync).toHaveBeenCalledOnce(); // sync is not retried
     expect(merge).not.toHaveBeenCalled();
     expect(erroredListener).toHaveBeenCalledOnce();
@@ -179,7 +181,9 @@ describe('Model', () => {
     );
     model.on('errored', erroredListener);
 
-    await model.sync().catch((err) => expect(err.message).toEqual('The sync function returned an undefined sequenceId'));
+    await model
+      .sync()
+      .catch((err) => expect(err.message).toEqual('The sync function returned an undefined sequenceId'));
     expect(sync).toHaveBeenCalledOnce();
     expect(merge).not.toHaveBeenCalled();
     expect(erroredListener).toHaveBeenCalledOnce();
