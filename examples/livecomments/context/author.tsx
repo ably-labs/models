c0bfc060897e7b40cd56c99b19609e41967a0a1b
Mike Christensen <mike.christensen@ably.com>
2024-05-28 12:06:35 +0100

    example: remove livecomments
    This is now at: https://github.com/ably-labs/live-comments

--- examples/livecomments/context/author.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext, ReactNode } from 'react';
-import type { Author as AuthorType } from '@/lib/prisma/api';
-
-export const AuthorContext = createContext<AuthorType | null>(null);
-
-export const AuthorProvider = ({ author, children }: { author: AuthorType; children: ReactNode }) => {
-  return <AuthorContext.Provider value={author}>{children}</AuthorContext.Provider>;
-};
